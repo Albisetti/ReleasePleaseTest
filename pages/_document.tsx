f1d011797fbde27c2adabce47d85978079f61dc6
Ivan Albisetti <37214460+Albisetti@users.noreply.github.com>
2023-04-19 15:24:46 -0300

    feat: Updated content

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div>aaaa</div>
+        <div>aaaaaaaaaa</div>
         <Main />
         <NextScript />
       </body>
