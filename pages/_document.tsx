a12d9a307309e17dbbde262dea7b01439178f8ea
Ivan Albisetti <37214460+Albisetti@users.noreply.github.com>
2023-04-19 13:43:06 -0300

    feat: Added more content to div

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div>aaaaaaaa</div>
+        <div>aaaaaaaaaaaaa</div>
         <Main />
         <NextScript />
       </body>
