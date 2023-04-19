d77ccee9bda0d6243e195b5b6e81423349871b3f
Ivan Albisetti <37214460+Albisetti@users.noreply.github.com>
2023-04-19 14:00:08 -0300

    feat: Removed div

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div>aaaaaaaaaaaaaaaaaa</div>
         <Main />
         <NextScript />
       </body>
