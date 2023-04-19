e3a38b85d951f22cf6cdb90d10a097d423832e60
Ivan Albisetti <37214460+Albisetti@users.noreply.github.com>
2023-04-19 15:28:10 -0300

    feat: aaaaaaaaa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div>aaaaaaaaaaaaa</div>
+        <div>asdsadasdsad</div>
         <Main />
         <NextScript />
       </body>
